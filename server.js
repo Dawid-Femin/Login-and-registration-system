82d88204a54c717e4e7efe93d56c64eea664633d
Dawid Femin <dawid.femin@gmail.com>
2022-11-30 11:45:13 +0100

    Created register and view for it

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+//Importing Libraies that I installed using npm
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt"); //Importing bcrypt package
+
+const users = [];
+
+app.use(express.urlencoded({extended: false}));
+
+app.post("/register", async(req,res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        console.log(users); //Display newly registered users in console
+        res.redirect("/login");
+    } catch (e) {
+        console.log(e);
+        res.redirect("/register");
+    }
+});
 
 //Routes
 app.get('/', (req,res) => {
@@ -14,5 +37,6 @@ app.get('/register', (req,res) => {
     res.render('register.ejs')
 });
 //End routes
+
 
 app.listen(3000);
